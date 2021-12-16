f8eabb31b7e71f4d838da4c67ecfc9f52b3e270d
SHUBHANSHUDUBEY1@gmail.com <SHUBHANSHUDUBEY1@gmail.com>
2021-12-16 10:39:43 +0000

    2 views

---
+++ models/Covid-19-Indonesia/PopulationByProvince.sql
@@ -0,0 +1,15 @@
+WITH Population AS
+    (
+        SELECT DISTINCT province,Population_Density
+        FROM FIVETRAN_INTERVIEW_DB.GOOGLE_SHEETS.COVID_19_INDONESIA_SHUBHANSHU_DUBEY
+        WHERE province IS NOT NULL
+    )
+
+
+,Total_Deaths AS
+(
+    SELECT * FROM {{ ref('TotalDeathsByProvince') }}
+)
+
+SELECT * FROM Population
+inner join Total_Deaths USING(province)
